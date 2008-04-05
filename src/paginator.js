7e62af4255a187a575d969e6af1f9c6aef2c46ea
Jose <jose@peleteiro.net>
2008-04-05 17:00:02 -0300

    Next/Pior with boundaries

--- src/paginator.js
+++ src/paginator.js
@@ -1,16 +1,15 @@
 var Paginator = Class.create({
   initialize: function(element) {
+		this.page_size = 10;
+		this.page_number = 1;
+	
     this.container_element = $(element);
     this.items = this.container_element.immediateDescendants();
-    this.page_size = 10;
-    this.page_number = 1;
-    
-    var n = 0;
-    this.items.each(function(i) {
-      n++;
-      if (n > this.page_size) i.hide();
-    }.bind(this));
-    
+		
+		// total pages
+    this.last_page = parseInt(this.items.size() / this.page_size);
+		if (this.items.size() % this.page_size) this.last_page++;
+
     this.pior_link = new Element('a', { href: '' }).update('Pior');
     Event.observe(this.pior_link, 'click', this._pior.bind(this));
     
@@ -24,32 +23,29 @@ var Paginator = Class.create({
     this.next_element = this.container_element.insert({
       bottom: new Element('li', { 'class': 'next' }).insert(this.next_link)
     });
+
+		this.page(1);
   },
   
   pior: function() {
-    if (this.page_number == 1) return false;
-    
-    this.page_number--;
-
-    var n = 0;
-    var first_index = (this.page_size * (this.page_number - 1));
-    var last_index = first_index + this.page_size - 1;
-    this.items.each(function(i) {
-      if (n < first_index || n > last_index) {
-        i.hide();
-      } else {
-        i.show();
-      }
-      n++;
-    }.bind(this));    
+    return this.page(this.page_number - 1);
   },
 
   next: function() {
-    this.page_number++;
+    this.page(this.page_number + 1);
+  },
 
+	page: function(number) {
+    if (number < 1) return false;
+		if (number > this.last_page) return false;
+
+		this.page_number = number;
     var n = 0;
     var first_index = (this.page_size * (this.page_number - 1));
     var last_index = first_index + this.page_size - 1;
+		
+		this._checkBoundaries();
+
     this.items.each(function(i) {
       if (n < first_index || n > last_index) {
         i.hide();
@@ -58,7 +54,23 @@ var Paginator = Class.create({
       }
       n++;
     }.bind(this));    
-  },
+
+		return true;
+	},
+
+	_checkBoundaries: function() {
+		if (this.page_number == 1) { 
+			this.pior_link.addClassName('disabled');
+		} else {
+			this.pior_link.removeClassName('disabled');
+		}
+
+		if (this.page_number == this.last_page) { 
+			this.next_link.addClassName('disabled');
+		} else {
+			this.next_link.removeClassName('disabled');
+		}
+	},
   
   _pior: function(ev) {
     try { this.pior(); } finally { ev.stop(); }
@@ -69,5 +81,3 @@ var Paginator = Class.create({
   }
 
 });
-
-
